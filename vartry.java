164ee0cf2841d4c989200bc0ffa8574fafda5288
bhadreshjani <nilkanthim@gmail.com>
2016-10-06 11:42:29 -0700

    Add files via upload
    the answer Is D . Null 0 0

---
+++ vartry.java
@@ -0,0 +1,14 @@
+
+public class vartry {
+	
+		int x; int y;
+		String name;
+		public static void main(String args[]) 
+		{
+		vartry pnt = new vartry();
+		System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+		 }
+}
+
+
+// answer is option D. null 0 0
